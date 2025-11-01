12be9db5eeb7de264782f0315c03261cb0122ed6
pham thi anh thu <phamthianhthu@MacBook-Air-cua-pham.local>
2025-11-01 22:21:45 +0700

    test: thêm test setup cho MongoDB connection và cleanup

---
+++ tests/setup/testSetup.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.test" });
+
+const TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || "mongodb://localhost:27017/drug_be_test";
+
+beforeAll(async () => {
+  await mongoose.connect(TEST_MONGODB_URI);
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+});
+
+beforeEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+});
+
