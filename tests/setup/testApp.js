149efb44c8a6b7333df5357595b738af753d2d0a
pham thi anh thu <phamthianhthu@MacBook-Air-cua-pham.local>
2025-11-01 22:22:53 +0700

    test: thêm test app setup cho integration testing

---
+++ tests/setup/testApp.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoutes from "../../routes/authRoutes.js";
+import userRoutes from "../../routes/userRoutes.js";
+
+dotenv.config({ path: ".env.test" });
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.use((err, req, res, next) => {
+  console.error("Lỗi:", err);
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Lỗi server",
+  });
+});
+
+export default app;
+
